0e35b306f0e3eabee71a1253efaddaa39f9be8fb
Vladislav <dezwink696@gmail.com>
2020-09-30 00:57:15 +0300

    Изменил первую и вторую фильтрацию

--- src/main/java/com/gridnine/testing/FlightFilter.java
+++ src/main/java/com/gridnine/testing/FlightFilter.java
@@ -2,43 +2,43 @@ package com.gridnine.testing;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlightFilter {
     static void removeFlightBeforNow(List<Flight> flights){
-        outer:
-        for (int flightNumbers = 0; flightNumbers < flights.size(); flightNumbers++) {
-            for (int seg = 0; seg < flights.get(flightNumbers).getSegments().size(); seg++) {
-                if (flights.get(flightNumbers).getSegments().get(seg).getDepartureDate().isBefore(LocalDateTime.now())) {
-                    flights.remove(flightNumbers);
-                    continue outer;
-                }
-            }
-        }
-        flightsString(flights);
+        flights
+                .stream()
+                .forEach( flight -> System.out.println(flight.getSegments()
+                        .stream()
+                        .filter(segment -> segment.getDepartureDate().isAfter(LocalDateTime.now()))
+                        .collect(Collectors.toList()))
+                );
+        System.out.println("\n");
     }
 
     static void removeFlightBackToThePast(List<Flight> flights){
-        outer:
-        for (int flightNumbers = 0; flightNumbers < flights.size(); flightNumbers++) {
-            for (int seg = 0; seg < flights.get(flightNumbers).getSegments().size(); seg++) {
-                if (flights.get(flightNumbers).getSegments().get(seg).getDepartureDate().isAfter(flights.get(flightNumbers).getSegments().get(seg).getArrivalDate())) {
-                    flights.remove(flightNumbers);
-                    continue outer;
-                }
-            }
-        }
-        flightsString(flights);
+        flights
+                .stream()
+                .forEach( flight -> System.out.println(flight.getSegments()
+                        .stream()
+                        .filter(segment -> segment.getDepartureDate().isBefore(segment.getArrivalDate()))
+                        .collect(Collectors.toList()))
+                );
+        System.out.println("\n");
     }
 
     static void removeFlightOnEarthMore2Hours(List<Flight> flights){
-        outer:
+        int hours = 0;
+
         for (int flightNumbers = 0; flightNumbers < flights.size(); flightNumbers++) {
             for (int seg = 0; seg + 1 < flights.get(flightNumbers).getSegments().size(); seg++) {
-                if (flights.get(flightNumbers).getSegments().get(seg).getArrivalDate().getHour()-flights.get(flightNumbers).getSegments().get(seg+1).getDepartureDate().getHour() > 2) {
-                    flights.remove(flightNumbers);
-                    continue outer;
-                }
+                hours += Math.abs(flights.get(flightNumbers).getSegments().get(seg).getArrivalDate().getHour()-flights.get(flightNumbers).getSegments().get(seg+1).getDepartureDate().getHour());
             }
+            if ( Math.abs(hours) > 2){
+                flights.remove(flightNumbers);
+                flightNumbers--;
+            }
+            hours = 0;
         }
         flightsString(flights);
     }
@@ -49,4 +49,5 @@ public class FlightFilter {
         }
         System.out.println("\n");
     }
+
 }
--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -2,10 +2,10 @@ package com.gridnine.testing;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
     public static void main(String[] args) {
         List<Flight> flights = new ArrayList<>(FlightBuilder.createFlights());
-
         FlightFilter.removeFlightBeforNow(flights);
         FlightFilter.removeFlightBackToThePast(flights);
         FlightFilter.removeFlightOnEarthMore2Hours(flights);
