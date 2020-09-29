2dd25c4f2e5d72cb8343a5bfb72300e200605136
Vladislav <dezwink696@gmail.com>
2020-09-30 01:19:56 +0300

    changed filters

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,13 +1,9 @@
 package com.gridnine.testing;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) {
-        List<Flight> flights = new ArrayList<>(FlightBuilder.createFlights());
-        FlightFilter.removeFlightBeforNow(flights);
-        FlightFilter.removeFlightBackToThePast(flights);
-        FlightFilter.removeFlightOnEarthMore2Hours(flights);
+        FlightFilter.removeFlightBeforNow(FlightBuilder.createFlights());
+        FlightFilter.removeFlightBackToThePast(FlightBuilder.createFlights());
+        FlightFilter.removeFlightOnEarthMore2Hours(FlightBuilder.createFlights());
     }
 }
