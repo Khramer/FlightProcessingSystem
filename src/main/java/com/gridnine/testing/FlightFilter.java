68a72aaee101c7751851e320e430fb9b7e5f8d80
Vladislav <dezwink696@gmail.com>
2020-09-30 01:13:25 +0300

    changed filters

--- src/main/java/com/gridnine/testing/FlightFilter.java
+++ src/main/java/com/gridnine/testing/FlightFilter.java
@@ -34,20 +34,10 @@ public class FlightFilter {
             for (int seg = 0; seg + 1 < flights.get(flightNumbers).getSegments().size(); seg++) {
                 hours += Math.abs(flights.get(flightNumbers).getSegments().get(seg).getArrivalDate().getHour()-flights.get(flightNumbers).getSegments().get(seg+1).getDepartureDate().getHour());
             }
-            if ( Math.abs(hours) > 2){
-                flights.remove(flightNumbers);
-                flightNumbers--;
+            if ( Math.abs(hours) <= 2){
+                System.out.println(flights.get(flightNumbers).getSegments().toString());
             }
             hours = 0;
         }
-        flightsString(flights);
     }
-
-    static void flightsString(List<Flight> flights){
-        for (int flightNumbers = 0; flightNumbers < flights.size(); flightNumbers++) {
-            System.out.println(flights.get(flightNumbers).toString());
-        }
-        System.out.println("\n");
-    }
-
 }
